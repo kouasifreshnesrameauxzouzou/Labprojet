d97a8535413cd4e474eb6c87da785f0f07e5fe91
Zouzou kouassi Freshnes <64736559+kouasifreshnesrameauxzouzou@users.noreply.github.com>
2024-02-13 21:06:12 +0000

    Update labprojet.py

--- labprojet.py
+++ labprojet.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
 from sklearn.model_selection import train_test_split
